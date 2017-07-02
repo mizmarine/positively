c0f599084cfc40853e8a96bbf41b1b9168c0e987
mizmarine <masa.guruto+marine@gmail.com>
2017-07-02 18:06:41 +0900

    add: diary page

---
+++ ui/positivery/src/components/Diary.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>にっきぺーじ</h1>
+    <div>{{text}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'diary',
+  data () {
+    return {
+      text: 'it is diary text'
+    }
+  }
+}
+</script>
