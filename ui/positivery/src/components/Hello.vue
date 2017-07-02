ed2032cdd5525abea6ff66e2177ef7814d537870
mizmarine <masa.guruto+marine@gmail.com>
2017-07-02 15:48:34 +0900

    add positive strings

--- ui/positivery/src/components/Hello.vue
+++ ui/positivery/src/components/Hello.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2> to be positive ! </h2>
     <h2>Essential Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
@@ -25,7 +26,7 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Positivery.'
     }
   }
 }
